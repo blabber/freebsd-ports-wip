2ff1943eabd87f35eac271e70605256efae30a50
Tobias Rehbein <tobias.rehbein@web.de>
2019-01-30 22:41:52 +0100

    Adapt security/pass-audit to changed sysutils/password-store

--- security/pass-audit/files/patch-audit.bash
+++ security/pass-audit/files/patch-audit.bash
@@ -1,13 +1,11 @@
---- audit.bash.orig	2019-01-27 16:58:22 UTC
+--- audit.bash.orig	2018-02-24 18:10:37 UTC
 +++ audit.bash
-@@ -16,8 +16,8 @@
- #    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+@@ -17,7 +17,7 @@
  #
  
--readonly CMD="audit"
+ readonly CMD="audit"
 -readonly LIBDIR="${PASSWORD_STORE_LIBDIR:-/usr/lib/password-store/$CMD}"
-+readonly CMD="pass-audit"
-+readonly LIBDIR="${PASSWORD_STORE_LIBDIR:-%%PREFIX%%/libexec}"
++readonly LIBDIR="${PASSWORD_STORE_LIBDIR:-%%PREFIX%%/libexec/password-store/pass-audit}"
  readonly Bold='\e[1m' Bred='\e[1;31m' reset='\e[0m'
  _error() { echo -e " ${Bred}[x]${reset} ${Bold}Error:${reset} ${*}" >&2; }
  _die() { _error "${@}" && exit 1; }
